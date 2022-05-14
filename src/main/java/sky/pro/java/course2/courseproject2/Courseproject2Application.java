1b4539a36d98b5aed4a22cdcdf794f3815a029ad
Ли Сергей <sergey.r.lee@gmail.com>
2022-05-14 14:10:27 +0300

    Комит к курсовой работе 2-го курса.

---
+++ src/main/java/sky/pro/java/course2/courseproject2/Courseproject2Application.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.courseproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courseproject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Courseproject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/java/course2/courseproject2/Courseproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.courseproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Courseproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
